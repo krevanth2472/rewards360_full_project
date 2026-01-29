71788508281fb2a6be927f064775f28fd185554c
Kamjula <2460671@cognizant.com>
2026-01-29 10:47:21 +0530

    Footer Style changed to center

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Footer(){
   return (
-    <footer>
+    <footer className="footer">
       <small>© {new Date().getFullYear()} Rewards360 — Project360 — Unified Loyalty</small>
     </footer>
   )
